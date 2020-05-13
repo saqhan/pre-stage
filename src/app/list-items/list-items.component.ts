b3335b4ddc3dcadfb7411fa55def78d707c53436
saqhan <saqhan1@mail.ru>
2020-05-13 15:32:42 +0300

    currect css

--- src/app/list-items/list-items.component.ts
+++ src/app/list-items/list-items.component.ts
@@ -14,16 +14,18 @@ export class ListItemsComponent implements OnInit {
         'At Froala, our mission has always been about pushing web editing beyond its conceived limits—this vision has served as the guiding philosophy for all Froala releases from Version 1.x to',
       img: 'https://via.placeholder.com/300x300?text=Visit',
       author: 'Kirti Joshi',
-      date: 'April 21, 2020'
+      date: 'April 21, 2020',
+      category: 'Editor'
     },
     {
       id: 2,
       title: 'Doing Wonders With Images In the Froala Editor',
       subTitle:
-        'Froala Editor can enhance your editing experience by providing methods to customize and manipulate creative images. Read more to learn what\'s possible!',
+        "Froala Editor can enhance your editing experience by providing methods to customize and manipulate creative images. Read more to learn what's possible!",
       img: 'https://via.placeholder.com/300x300?text=Visit',
       author: 'Kirti Joshi',
-      date: 'April 21, 2020'
+      date: 'April 21, 2020',
+      category: 'Editor'
     },
     {
       id: 3,
@@ -32,16 +34,18 @@ export class ListItemsComponent implements OnInit {
         'At Froala, our mission has always been about pushing web editing beyond its conceived limits—this vision has served as the guiding philosophy for all Froala releases from Version 1.x to',
       img: 'https://via.placeholder.com/300x300?text=Visit',
       author: 'Kirti Joshi',
-      date: 'April 21, 2020'
+      date: 'April 21, 2020',
+      category: 'Editor'
     },
     {
       id: 4,
       title: 'Using Multiple Froala Editors On a Single Webpage',
       subTitle:
-        'Froala Editor can enhance your editing experience by providing methods to customize and manipulate creative images. Read more to learn what\'s possible!',
+        "Froala Editor can enhance your editing experience by providing methods to customize and manipulate creative images. Read more to learn what's possible!",
       img: 'https://via.placeholder.com/300x300?text=Visit',
       author: 'Kirti Joshi',
-      date: 'April 21, 2020'
+      date: 'April 21, 2020',
+      category: 'Editor'
     },
   ];
 
